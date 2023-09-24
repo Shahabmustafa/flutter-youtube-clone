41842f0ea704371ef785dcc2b8732d702bc3f6e4
Shahab Mustafa <shahabmustafa57@gmail.com>
2023-09-24 20:27:34 +0500

    set code for youtube clone

---
+++ lib/view/login/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 40,
+            width: 300,
+
+          ),
+        ],
+      ),
+    );
+  }
+}
