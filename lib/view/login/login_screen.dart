4416caa994684d2ffcc4f0cec727d4ca6ef71960
Shahab Mustafa <shahabmustafa57@gmail.com>
2023-09-26 16:24:07 +0500

    add login screen

--- lib/view/login/login_screen.dart
+++ lib/view/login/login_screen.dart
@@ -1,4 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../navigation.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,15 +14,56 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 40,
-            width: 300,
-
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/youtube.png",height: 50,width: 50,),
+                SizedBox(
+                  width: size.width * 0.01,
+                ),
+                Text("YOUTUBE",style: GoogleFonts.aBeeZee(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  letterSpacing: 2,
+                ),)
+              ],
+            ),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => BottomNavigation()));
+              },
+              child: Container(
+                height: 55,
+                width: 300,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey,width: 2),
+                  borderRadius: BorderRadius.circular(50)
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/images/search.png",height: 30,width: 30,),
+                    SizedBox(
+                      width: size.width * 0.08,
+                    ),
+                    Text("Sign in Google",style: GoogleFonts.aBeeZee(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),)
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
